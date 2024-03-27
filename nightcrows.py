e84925475d8b317f364b4d98005983ae4befda71
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2024-03-28 00:14:39 +0700

    Add files via upload

---
+++ nightcrows.py
@@ -0,0 +1,21 @@
+# nightcrows simple launcher
+# by fahmiyufrizal@2024
+
+import os
+import subprocess
+from os import path
+import sys
+from sys import exit
+import time
+import pathlib
+
+# parameters
+version = 'v1.1.3'
+titlewindow = 'fahmiyufrizal@2024 [github.com/fahmiyufrizal]'
+NightFN = (r'NightCrows_NetCafe_Launcher.exe')
+NightLN = (r'launcher\NightCrowsGLauncher.exe')
+NightLN_Dicky = (r'Portable\NightCrowsG\launcher\NightCrowsGLauncher.exe')
+dp0 = os.getcwd()
+
+pathlib.Path("_Reg_").mkdir(parents=True, exist_ok=True)
+exit()
